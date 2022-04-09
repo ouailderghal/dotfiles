37fec47d2c055a756e52eac68f87355676952164
Ouail Derghal <ouailderghal@gmail.com>
2022-04-09 23:30:09 +0100

    Update clean startup in defaults.el

--- emacs/defaults.el
+++ emacs/defaults.el
@@ -5,8 +5,8 @@
 
 (defun defaults/clean-startup ()
   "Remove toolbar, menubar and scrollbar"
-  (tool-bar-mode 0)
-  (menu-bar-mode 0)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
   (scroll-bar-mode -1))
 
 (defun defaults/append-new-line ()
