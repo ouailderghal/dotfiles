f976df92c3d8de95490e94833f6f35c95f429122
Ouail Derghal <ouailderghal1@gmail.com>
2023-01-21 19:54:20 +0100

    awesome: update default terminal

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -48,7 +48,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
+terminal = "st"
 editor = os.getenv("EDITOR") or "vi"
 editor_cmd = terminal .. " -e " .. editor
 
