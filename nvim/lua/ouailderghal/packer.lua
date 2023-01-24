62448001cb4bf4712a7e77512dce0b1f726d2bec
Ouail Derghal <ouailderghal1@gmail.com>
2023-01-24 20:37:19 +0100

    nvim: add telescope.nvim

--- nvim/lua/ouailderghal/packer.lua
+++ nvim/lua/ouailderghal/packer.lua
@@ -7,5 +7,11 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'jnurmine/Zenburn'
+  
+  use {
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.1', 
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 
 end)
