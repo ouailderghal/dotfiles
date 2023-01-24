b8bcf2b1940027bafc1e53f49d3666db96420d57
Ouail Derghal <ouailderghal1@gmail.com>
2023-01-24 20:10:50 +0100

    nvim: add colors.lua

--- nvim/lua/core/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-local theme = "zenburn"
-local status, _ = pcall(vim.cmd, string.format("colorscheme %s", theme))
-
-if not status then
-  print(string.format("Colorscheme %s not found.", theme))
-  return
-end
-
