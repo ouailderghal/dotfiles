201f533ab3d947b44e34ea5dea9670157b86c164
Ouail Derghal <ouailderghal1@gmail.com>
2023-01-31 22:54:38 +0100

    nvim: fix lualine theme

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'zenburn',
+    theme = 'auto',
     component_separators = '|',
     section_separators = '',
   },
