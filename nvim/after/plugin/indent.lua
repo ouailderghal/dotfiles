70e2b60983f000121803d7acff7117b2c56c23f2
Ouail Derghal <ouailderghal1@gmail.com>
2023-02-01 15:43:25 +0100

    nvim: change indent sign

--- nvim/after/plugin/indent.lua
+++ nvim/after/plugin/indent.lua
@@ -1,9 +1,8 @@
 vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "eol:↴"
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
+require('indent_blankline').setup {
+    char = '┊',
+    show_trailing_blankline_indent = false,
     show_current_context = true,
     show_current_context_start = true,
 }
