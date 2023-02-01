f52673912184281aec2bc569cf68f278da312e69
Ouail Derghal <ouailderghal1@gmail.com>
2023-02-01 15:44:36 +0100

    nvim: add space and eol signs

--- nvim/after/plugin/indent.lua
+++ nvim/after/plugin/indent.lua
@@ -1,5 +1,8 @@
 vim.opt.list = true
 
+-- vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "eol:↴"
+
 require('indent_blankline').setup {
     char = '┊',
     show_trailing_blankline_indent = false,
