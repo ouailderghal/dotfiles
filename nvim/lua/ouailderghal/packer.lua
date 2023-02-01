2e05c9673145cc4ae39a16b115074ee874f15d69
Ouail Derghal <ouailderghal1@gmail.com>
2023-02-01 21:41:27 +0100

    nvim: update lsp.lua

--- nvim/lua/ouailderghal/packer.lua
+++ nvim/lua/ouailderghal/packer.lua
@@ -4,62 +4,69 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'jnurmine/Zenburn'
-  use "lukas-reineke/indent-blankline.nvim"
-  use 'b3nj5m1n/kommentary'
-  use 'nvim-lualine/lualine.nvim'
-  use 'lewis6991/gitsigns.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use 'jnurmine/Zenburn'
+    use "lukas-reineke/indent-blankline.nvim"
+    use 'b3nj5m1n/kommentary'
+    use 'nvim-lualine/lualine.nvim'
+    use 'lewis6991/gitsigns.nvim'
 
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb'
+    use 'tpope/vim-fugitive'
+    use 'tpope/vim-rhubarb'
 
-  use {
-    'theprimeagen/harpoon' ,
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-
-  use {
-    'akinsho/toggleterm.nvim',
-    tag = '*',
-    config = function() require("toggleterm").setup() end
-  }
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lua'},
-
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+    use {
+        'theprimeagen/harpoon',
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
-  }
+
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+
+    use {
+        'akinsho/toggleterm.nvim',
+        tag = '*',
+        config = function() require("toggleterm").setup() end
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    use {
+        -- LSP Configuration & Plugins
+        'neovim/nvim-lspconfig',
+        requires = {
+            -- Automatically install LSPs to stdpath for neovim
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+
+            -- Useful status updates for LSP
+            'j-hui/fidget.nvim',
+
+            -- Additional lua configuration, makes nvim stuff amazing
+            'folke/neodev.nvim',
+        },
+    }
+
+    use { -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lua',
+            'rafamadriz/friendly-snippets'
+        }
+    }
 
 end)
