78627de0d875d5930b24026d42d3e0d74d60bda7
Ouail Derghal <ouailderghal1@gmail.com>
2023-06-24 16:24:16 +0100

    nvim: add trouble plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,20 +1,19 @@
 local Plugins = {
-  {'tpope/vim-fugitive'},
-  {'wellle/targets.vim'},
-  {'tpope/vim-repeat'},
-  {'kyazdani42/nvim-web-devicons', lazy = true},
-  {'numToStr/Comment.nvim', config = true, event = 'VeryLazy'},
-	{'aklt/plantuml-syntax'},
+	{ 'tpope/vim-fugitive' },
+	{ 'wellle/targets.vim' },
+	{ 'tpope/vim-repeat' },
+	{ 'kyazdani42/nvim-web-devicons', lazy = true },
+	{ 'numToStr/Comment.nvim',        config = true, event = 'VeryLazy' },
+	{ 'aklt/plantuml-syntax' },
+	{ 'nvim-lua/plenary.nvim' },
+	{ 'ThePrimeagen/harpoon' },
 
-	{'nvim-lua/plenary.nvim'},
-	{'ThePrimeagen/harpoon'},
-
-  -- Themes
-  {'folke/tokyonight.nvim'},
-  {'joshdick/onedark.vim'},
-  {'tanvirtin/monokai.nvim'},
-  {'lunarvim/darkplus.nvim'},
-	{'phha/zenburn.nvim'},
+	-- Themes
+	{ 'folke/tokyonight.nvim' },
+	{ 'joshdick/onedark.vim' },
+	{ 'tanvirtin/monokai.nvim' },
+	{ 'lunarvim/darkplus.nvim' },
+	{ 'phha/zenburn.nvim' },
 }
 
 return Plugins
