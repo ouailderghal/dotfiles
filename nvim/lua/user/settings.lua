6e834c64737f5e97dcb59198b3adc629fcd48699
Ouail Derghal <ouailderghal1@gmail.com>
2023-07-15 23:20:53 +0100

    nvim: add transparent background

--- nvim/lua/user/settings.lua
+++ nvim/lua/user/settings.lua
@@ -11,5 +11,12 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.signcolumn = 'yes'
 
+-- Transparent background
+vim.api.nvim_command([[
+    augroup ChangeBackgroudColour
+        autocmd colorscheme * :hi normal guibg=none
+    augroup END
+]])
+
 vim.opt.termguicolors = true
 
