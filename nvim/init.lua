e9a35e6506c5131f9f792ff895cb80f9d0af18df
Ouail Derghal <ouailderghal1@gmail.com>
2023-10-13 16:58:01 +0100

    nvim: enable icons in lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -167,7 +167,7 @@ require('lazy').setup({
         -- See `:help lualine.txt`
         opts = {
             options = {
-                icons_enabled = false,
+                icons_enabled = true,
                 theme = 'gruvbox',
                 component_separators = '|',
                 section_separators = '',
