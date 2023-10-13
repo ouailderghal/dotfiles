c81cf80687c22d9b21c9ada0f5593326f304cf29
Ouail Derghal <ouailderghal1@gmail.com>
2023-10-13 12:11:36 +0000

    nvim: add buffer delete keybinding

--- nvim/init.lua
+++ nvim/init.lua
@@ -566,6 +566,7 @@ local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
 
 keymap("n", "<leader>gg", ":Git<CR>", default_opts)
+keymap("n", "<leader>bd", ":bd<CR>", default_opts)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
