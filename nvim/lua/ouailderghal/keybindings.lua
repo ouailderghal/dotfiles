41356032e12cbcd792e6921bca6812179c60daf4
Ouail Derghal <ouailderghal1@gmail.com>
2023-10-13 17:48:02 +0100

    nvim: update keybindings.lua

--- nvim/lua/ouailderghal/keybindings.lua
+++ nvim/lua/ouailderghal/keybindings.lua
@@ -1,3 +1,12 @@
+-- [[ Keymaps ]]
+
+-- Keymaps for better default experience
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 -- Custom bindings
 vim.keymap.set('n', '<leader>gg', '<cmd>:Git<cr>')
 vim.keymap.set('n', '<leader>bd', '<cmd>:bdelete<cr>')
