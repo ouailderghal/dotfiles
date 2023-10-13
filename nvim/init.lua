1cbb3d974a7f5a274942dd8087305da16cfde135
Ouail Derghal <ouailderghal1@gmail.com>
2023-10-13 17:07:53 +0100

    nvim: add custom keybindings file

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,6 +40,7 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+--
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -578,15 +579,7 @@ cmp.setup {
     },
 }
 
--- Custom bindings
-local keymap = vim.api.nvim_set_keymap
-local default_opts = { noremap = true, silent = true }
--- local expr_opts = { noremap = true, expr = true, silent = true }
-
-keymap("n", "<leader>gg", ":Git<CR>", default_opts)
-keymap("n", "<leader>bd", ":bd<CR>", default_opts)
-keymap("n", "<leader>qq", ":q!<CR>", default_opts)
-keymap("n", "<leader>op", ":Neotree toggle<CR>", default_opts)
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+require("ouailderghal.keybindings")
---
+++ nvim/lua/ouailderghal/keybindings.lua
@@ -0,0 +1,6 @@
+-- Custom bindings
+vim.keymap.set('n', '<leader>gg', '<cmd>:Git<cr>')
+vim.keymap.set('n', '<leader>bd', '<cmd>:bdelete<cr>')
+vim.keymap.set('n', '<leader>qq', '<cmd>:quit!<cr>')
+vim.keymap.set('n', '<leader>op', '<cmd>:Neotree toggle<cr>')
+
