0e37240688a03d50fcd751d32d2615e39b97266b
Ouail Derghal <ouailderghal1@gmail.com>
2023-10-23 00:37:39 +0100

    nvim: update fugitive keybinding to open in new tab

--- nvim/lua/ouailderghal/keybindings.lua
+++ nvim/lua/ouailderghal/keybindings.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Custom bindings
-vim.keymap.set('n', '<leader>gg', '<cmd>:Git<cr>')
+vim.keymap.set('n', '<leader>gg', '<cmd>:tab Git<cr>')
 vim.keymap.set('n', '<leader>bd', '<cmd>:bdelete<cr>')
 vim.keymap.set('n', '<leader>qq', '<cmd>:quit!<cr>')
 vim.keymap.set('n', '<leader>op', '<cmd>:Neotree toggle<cr>')
