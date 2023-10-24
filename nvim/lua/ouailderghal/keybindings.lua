2cefe5790fdb50d10a5b80b8d2523f17a7df7703
Ouail Derghal <ouailderghal1@gmail.com>
2023-10-24 19:28:57 +0100

    nvim: update keybindings

--- nvim/lua/ouailderghal/keybindings.lua
+++ nvim/lua/ouailderghal/keybindings.lua
@@ -13,12 +13,15 @@ vim.keymap.set('n', '<leader>gl', '<cmd>:tab Git log<cr>', { desc = 'Open Git lo
 vim.keymap.set('n', '<leader>glo', '<cmd>:tab Git log --oneline<cr>', { desc = 'Open Git log in one line' })
 vim.keymap.set('n', '<leader>gp', '<cmd>:tab Git push<cr>', { desc = 'Git push' })
 
+-- Telescope bindings
+vim.keymap.set('n', '<leader>po', '<cmd>:Neotree toggle<cr>', { desc = 'Open Project Tree' })
+vim.keymap.set('n', '<leader>pf', require('telescope.builtin').git_files, { desc = 'Search [P]roject [F]iles' })
+
 -- Custom bindings
 vim.keymap.set('n', '<leader>bd', '<cmd>:bdelete<cr>')
 vim.keymap.set('n', '<leader>qq', '<cmd>:quit!<cr>')
 vim.keymap.set('n', '<leader>op', '<cmd>:Neotree toggle<cr>')
-
 vim.keymap.set('n', '<C-n>', '<cmd>:bn<cr>')
 vim.keymap.set('n', '<C-p>', '<cmd>:bp<cr>')
-
-vim.keymap.set('n', '<leader>zz', '<cmd>:ZenMode<cr>', { desc = 'Toggle Zen mode'})
+vim.keymap.set('n', '<leader>zz', '<cmd>:ZenMode<cr>', { desc = 'Toggle Zen mode' })
+vim.keymap.set('n', '<leader>ff', '<cmd>:Format<cr>', { desc = '[F]ormat [F]ile'})
