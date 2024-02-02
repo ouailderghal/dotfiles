4a99a9e156ea59aa6dae8af492b684c7138fa908
Ouail Derghal <ouail.derghal@imt-atlantique.fr>
2024-02-02 22:40:35 +0100

    scripts: update tmux-sessionizer.sh

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Projects ~/Projects/personal ~/Projects/phd ~/Projects/work ~/Projects/tmp -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/Projects/personal ~/Projects/phd ~/Projects/work ~/Projects/tmp -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
